8c63a0c8ad4ff8f06133c8c1d53c9de6681100a7
abrarsaleeem <abrarsaleem.info@gmail.com>
2025-11-24 03:47:56 +0500

    inital gradient page

---
+++ script.js
@@ -0,0 +1,49 @@
+let button1 = document.getElementById("leftBtn");
+let button2 = document.getElementById("rightBtn");
+let gradDisplay = document.getElementById("displayGrad");
+let copyBox = document.querySelector(".copyCode");
+let copyBoxText = document.getElementById("copyText");
+
+let hexValues = () => {
+    let myHexValues = "0123456789abcdef";
+    let colors = "#";
+    for(let i = 0; i < 6; i++){
+        colors = `${colors}${myHexValues[Math.floor(Math.random() * 16)]}`;
+    }
+    return colors;
+};
+
+let rgb1 = "#0a7a63";
+let rgb2 = "#9b0f12";
+
+const handleButton1 = () => {
+    rgb1 = hexValues();
+    button1.innerText = rgb1;
+    copyBoxText.innerHTML = `background-image: linear-gradient(to right, ${rgb1}, ${rgb2})`;
+    gradDisplay.style.backgroundImage = `linear-gradient(to right, ${rgb1}, ${rgb2})`;
+};
+
+const handleButton2 = () => {
+    rgb2 = hexValues();
+    button2.innerText = rgb2;
+    copyBoxText.innerHTML = `background-image: linear-gradient(to right, ${rgb1}, ${rgb2})`;
+    gradDisplay.style.backgroundImage = `linear-gradient(to right, ${rgb1}, ${rgb2})`;
+};
+
+const handleCopyBox = () => {
+    navigator.clipboard.writeText(copyBoxText.innerText);
+
+    copyBoxText.innerText = "Copied!";
+    copyBox.style.boxShadow = "0 0 4px #9f9d9dff";
+    copyBox.style.color = "lime";
+
+    setTimeout(() => {
+        copyBoxText.innerText = `background-image: linear-gradient(to right, ${rgb1}, ${rgb2})`;
+        copyBox.style.color = "#f1f1f1";
+        copyBox.style.boxShadow = "none";
+    }, 1200);
+};
+
+button1.addEventListener('click', handleButton1);
+button2.addEventListener('click', handleButton2);
+copyBox.addEventListener('click', handleCopyBox);
